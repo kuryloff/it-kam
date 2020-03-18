38ae981b4444ad9d0f0b54c3c8b69b318dc59371
IhorKurylov <iikurylov@gmail.com>
2020-03-18 19:12:53 +0200

    41

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,15 +4,14 @@ import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Nav";
 import Profile from "./components/Profile/Profile";
 import Dialogs from "./components/Dialogs/Dialogs";
-import {BrowserRouter, Route} from "react-router-dom";
 import News from './components/News/News'
 import Music from './components/Music/Music'
 import Settings from './components/Settings/Settings'
+import {Route} from "react-router-dom";
 
 
 const App = (props) => {
     return (
-        // <BrowserRouter>
             <div className='wrapper'>
                 <Header/>
                 <Navbar data={props.state.sidebar}/>
@@ -28,9 +27,7 @@ const App = (props) => {
                     <Route path={'/music'} render={() => <Music/>}/>
                     <Route path={'/settings'} render={() => <Settings/>}/>
                 </div>
-
             </div>
-        // </BrowserRouter>
     );
 };
 
--- my-app/src/redux/dialogs-reducer.js
+++ my-app/src/redux/dialogs-reducer.js
@@ -4,16 +4,16 @@ const UPDATE_NEW_MESSAGE_TEXT = 'UPDATE-NEW-MESSAGE-TEXT';
 const dialogsReducer = (state, action) => {
     switch (action.type) {
         case UPDATE_NEW_MESSAGE_TEXT:
-            state.newMessageText = action.body;
+            state.newMessageBody = action.body;
             return state;
         case
         ADD_MESSAGE:
             let newMessage = {
                 id: 8,
-                message: state.newMessageText,
+                message: state.newMessageBody,
             };
             state.messages.push(newMessage);
-            state.newMessageText = '';
+            state.newMessageBody = '';
             return state;
         default:
             return state;
