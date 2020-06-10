26bb1eafd4c91591e2958b51a10c7ffa987b98f9
IhorKurylov <iikurylov@gmail.com>
2020-06-10 10:20:17 +0300

    95 gh pages after deploy

---
+++ my-app/build/static/js/9.c8186b51.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpmy-app"]=this["webpackJsonpmy-app"]||[]).push([[9],{300:function(e,n,t){"use strict";t.r(n);var p=t(0),a=t.n(p);n.default=function(){return a.a.createElement("div",null,"News here")}}}]);
+//# sourceMappingURL=9.c8186b51.chunk.js.map
